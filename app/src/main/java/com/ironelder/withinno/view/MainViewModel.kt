b47db00f9a01fb331a235403c0c0ef26c01ce813
Jason <ironelder@gmail.com>
2020-04-25 16:42:41 +0900

    뷰모델 불필요한 코드 정리
    뷰모델 불필요한 코드 정리

--- app/src/main/java/com/ironelder/withinno/view/MainViewModel.kt
+++ app/src/main/java/com/ironelder/withinno/view/MainViewModel.kt
@@ -1,48 +1,25 @@
 package com.ironelder.withinno.view
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.ironelder.withinno.BuildConfig
-import com.ironelder.withinno.data.model.CrawlingResult
+import com.ironelder.withinno.domain.model.CrawlingDomainModel
 import com.ironelder.withinno.domain.repository.WebCrawlingRepositoryImpl
-import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
-import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.jsoup.nodes.Element
-import java.io.IOException
 
 class MainViewModel : ViewModel() {
-    private var clickCount: Int = 0
-    val countLiveData = MutableLiveData<Int>()
+
     private val disposeBag = CompositeDisposable()
-    open fun getInitialcount() {
-        webCrawling()
-    }
+    val webCrawlingData = MutableLiveData<List<CrawlingDomainModel>>()
 
-    private fun getJsoupDocument(): Observable<Document> {
-        return Observable.create {
-            try {
-                val document = Jsoup.connect(BuildConfig.BASE_URL).get()
-                it.onNext(document)
-            } catch (e: IOException) {
-                it.onError(e)
-            } finally {
-                it.onComplete()
-            }
-        }
-    }
-
-    fun webCrawling() {
+    fun startWebCrawling() {
         disposeBag.add(
             WebCrawlingRepositoryImpl.getRemoteWebCrawlingData()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe {
-                    Log.d("ironelderLog", "result yes = ${it}")
+                    webCrawlingData.postValue(it)
                 })
     }
 
@@ -50,4 +27,5 @@ class MainViewModel : ViewModel() {
         super.onCleared()
         disposeBag.dispose()
     }
+
 }
