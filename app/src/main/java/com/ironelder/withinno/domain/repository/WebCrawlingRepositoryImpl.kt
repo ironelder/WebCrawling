5395d710af671587464fcb40fa3a5cd396c9a801
Jason <ironelder@gmail.com>
2020-04-25 16:48:52 +0900

    매핑 코드 분리
    매핑 코드 분리

--- app/src/main/java/com/ironelder/withinno/domain/repository/WebCrawlingRepositoryImpl.kt
+++ app/src/main/java/com/ironelder/withinno/domain/repository/WebCrawlingRepositoryImpl.kt
@@ -1,15 +1,21 @@
 package com.ironelder.withinno.domain.repository
 
+import com.ironelder.withinno.data.model.CrawlingDataModel
 import com.ironelder.withinno.data.remote.source.RemoteDataSourceImpl
 import com.ironelder.withinno.domain.model.CrawlingDomainModel
 import io.reactivex.Observable
 
 object WebCrawlingRepositoryImpl : WebCrawlingRepository {
+
     override fun getRemoteWebCrawlingData(): Observable<List<CrawlingDomainModel>> {
         return RemoteDataSourceImpl.getRemoteCrawlingData().map {
-            it.datas.map { cr ->
-                CrawlingDomainModel(title = cr.title, imgUrl = cr.imgUrl)
-            }
+            dataToDomain(it)
+        }
+    }
+
+    private fun dataToDomain(data:CrawlingDataModel):List<CrawlingDomainModel>{
+        return data.datas.map { cr ->
+            CrawlingDomainModel(title = cr.title, imgUrl = cr.imgUrl)
         }
     }
 
