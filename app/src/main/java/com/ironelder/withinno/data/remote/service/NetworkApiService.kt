ac2ab501ccfd6991871a5295b264bb6a392ceb39
Jason <ironelder@gmail.com>
2020-04-25 16:16:14 +0900

    불필요한 서비스 삭제 및 API서비스 수정
    불필요한 서비스 삭제 및 API서비스 수정

---
+++ app/src/main/java/com/ironelder/withinno/data/remote/service/NetworkApiService.kt
@@ -0,0 +1,25 @@
+package com.ironelder.withinno.data.remote.service
+
+import com.ironelder.withinno.BuildConfig
+import io.reactivex.Observable
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
+import java.io.IOException
+
+object NetworkApiService {
+    val crawlingApi: Observable<Document> by lazy {
+        return@lazy Observable.create {
+            try {
+                val document = Jsoup.connect(BuildConfig.BASE_URL).get()
+                it.onNext(document)
+            } catch (e: IOException) {
+                it.onError(e)
+            } finally {
+                it.onComplete()
+            }
+        }
+    }
+
+}
+
+
